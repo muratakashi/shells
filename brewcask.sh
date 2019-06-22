35307a9aa5659c19ccbab5b8c958bbc7fdeafddd
muratakashi <muratakashi@gmail.com>
2019-06-22 09:50:40 +0900

    不要にしたソフトウェアをコメントアウトしました

--- brewcask.sh
+++ brewcask.sh
@@ -2,5 +2,5 @@ brew cask macvim
 brew cask visual-studio-code
 brew cask pgadmin4
 brew cask install google-chrome
-brew cask install java
-brew cask install docker
+# brew cask install java
+# brew cask install docker
