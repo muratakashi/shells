fc5ca3ef0f59f627b6f5e28ea96eec71acfe7b99
muragoya <muragoya@mt003.local>
2020-07-09 22:54:39 +0900

    現状の必要な設定に更新しました

--- brewcask.sh
+++ brewcask.sh
@@ -1,6 +1,6 @@
 brew cask macvim
 brew cask visual-studio-code
-brew cask pgadmin4
+#brew cask pgadmin4
 brew cask install google-chrome
 # brew cask install java
 # brew cask install docker
--- initial-setting.sh
+++ initial-setting.sh
@@ -1,8 +1,9 @@
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install go
+brew install python@3.8
 
 source brewcask.sh
 
 # golang パッケージ管理ツールのインストール
-go get golang.org/x/vgo
+# go get golang.org/x/vgo
